cb1cc2d6de1ced1edb155f9ff1516c00ceee5fa3
Andre Ambrosio Boechat <boechat107@gmail.com>
2015-05-24 22:05:05 -0300

    It doesn't fetch the same url twice, but it's not parallel

--- exercise-web-crawler.go
+++ exercise-web-crawler.go
@@ -10,29 +10,54 @@ type Fetcher interface {
 	Fetch(url string) (body string, urls []string, err error)
 }
 
+type Cache map[string]string
+
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
-func Crawl(url string, depth int, fetcher Fetcher) {
-	// TODO: Fetch URLs in parallel.
-	// TODO: Don't fetch the same URL twice.
-	// This implementation doesn't do either:
-	if depth <= 0 {
-		return
+func Crawl(url string, depth int, fetcher Fetcher, c chan Cache) {
+	// Only one goroutine have access to this data at some moment.
+	cache := <-c
+	//fmt.Printf("%s: %v\n", url, cache)
+	if depth > 0 {
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("found: %s %q\n", url, body)
+			var newurls []string
+			for _, u := range urls {
+				if _, status := cache[u]; !status {
+					cache[u] = "done"
+					newurls = append(newurls, u)
+				}
+			}
+			if len(newurls) > 0 {
+				children := make(chan Cache)
+				for _, u := range newurls {
+					go Crawl(u, depth-1, fetcher, children)
+				}
+				// Now all goroutines are waiting for some value on "children".
+				children <- cache
+				// The parent goroutine will be the last to have access to the channel.
+				<-children
+			}
+		}
 	}
-	body, urls, err := fetcher.Fetch(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("found: %s %q\n", url, body)
-	for _, u := range urls {
-		Crawl(u, depth-1, fetcher)
-	}
+	// Now it's safe to signal that the job is done.
+	c <- cache
 	return
 }
 
 func main() {
-	Crawl("http://golang.org/", 4, fetcher)
+	url := "http://golang.org/"
+	c := make(chan Cache)
+	go Crawl(url, 4, fetcher, c)
+	// To send data through the channel, first we need to have somebody waiting to
+	// read.
+	c <- Cache{url: "done"}
+	// Now we wait the goroutine finish its job and send something back
+	// (synchronization).
+	<-c
 }
 
 // fakeFetcher is Fetcher that returns canned results.
